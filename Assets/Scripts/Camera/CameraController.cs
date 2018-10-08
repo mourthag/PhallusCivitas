4b8a3eb0901681d5ab32e59d446757481585debd
Julian Spittel <julispittel@gmail.com>
2018-10-08 21:07:36 +0200

    Add fancy camera controller Add more Inputs Code cleanup

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Entities;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _minCameraSize;
+    [SerializeField] private float _maxCameraSize;
+    [SerializeField] private Camera _camera;
+    private Vector3 _centerposition;
+    
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var players = FindObjectsOfType<Player>();
+        _centerposition = Vector3.zero;
+	    float maxDistance = 0;
+
+	    for (var index = 0; index < players.Length; index++)
+	    {
+	        var player = players[index];
+	        _centerposition += player.transform.position;
+
+	        for (var i = index; i < players.Length; i++)
+	        {
+	            var otherPlayer = players[i];
+	            var dist = Vector3.Distance(player.transform.position, otherPlayer.transform.position);
+
+                if (dist > maxDistance)
+                {
+                    maxDistance = dist;
+                }
+	        }
+	    }
+	    if (players.Length > 0)
+	    {
+	        _centerposition = _centerposition / players.Length;
+        }
+        
+
+	    var size = Mathf.Clamp( maxDistance / 2, _minCameraSize, _maxCameraSize);
+	    _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, size, 0.1f);
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, new Vector3(_centerposition.x, 0, -10), .2f);
+
+
+        //TODO: Restrict cameraposition to screen
+	}
+
+}
--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -8,11 +8,8 @@ namespace Entities
     public class Player : MonoBehaviour
     {
         [SerializeField] private PlayerControllerBase _controller;
-        [SerializeField] public SpriteRenderer _sprite;
-        [SerializeField] public PlayerAnimation Animation;
-        [SerializeField] public SpriteRenderer _playerIdSprite;
 
-        private int _playerId = 1;
+        [SerializeField] private int _playerId = 1;
 
         public int PlayerId
         {
@@ -24,17 +21,5 @@ namespace Entities
         }
 
         public void LookLeft() { _controller.UpdateViewDirection(new Vector2(-1,0)); }
-        public Color Color { set { _sprite.color = value; } }
-        
-
-        void Start()
-        {
-           
-        }
-
-        public PlayerControllerBase GetPlayerController()
-        {
-            return _controller;
-        }
     }
 }
