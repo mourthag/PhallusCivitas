3c70b067f3214b0c94503abc4d312dd4992a9476
kaiserjonathan <jonathankaiservevo@gmail.com>
2018-10-08 18:46:20 +0200

    idk

---
+++ Assets/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Projectile : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 _projectileSpeed;
+
+    public void SetSpeed(Vector3 projectileSpeed)
+    {
+        _projectileSpeed = projectileSpeed;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    transform.position +=  _projectileSpeed * Time.deltaTime;
+
+	}
+}
--- Assets/Scripts/Control/Actions/BlaneSpecial.cs
+++ Assets/Scripts/Control/Actions/BlaneSpecial.cs
@@ -1,19 +1,58 @@
 ﻿
 
+using UnityEngine;
+
 namespace Control.Actions
 {
-    public class BlaneSpecial :  Action {
-
-
+    public class BlaneSpecial :  Action
+    {
+        [SerializeField] private PlayerControllerBase _playerController;
+        [SerializeField] private GameObject _projectilePrefab;
 
         public override void TryToActivate(Direction direction)
         {
-            throw new System.NotImplementedException();
+            if (IsOnCooldown) return;
+
+            base.TryToActivate(direction);
+            _playerController.DisableInput();
+            PlayerAnimation.UseSkill();
         }
 
         public override void Activate(Direction direction)
         {
-            throw new System.NotImplementedException();
+            ShootProjectile(direction);
+        }
+
+        public void ShootProjectile(Direction direction)
+        {
+            _playerController.EnableInput();
+
+            Vector2 projectileSpeed;
+            Quaternion rotation = Quaternion.identity;
+
+            switch (direction)
+            {
+                case Direction.LEFT:
+                    projectileSpeed = Vector2.left;
+                    rotation = Quaternion.Euler(0, 0, 180);
+                    break;
+                case Direction.RIGHT:
+                    projectileSpeed = Vector2.right;
+
+                    break;
+                case Direction.TOP:
+                    projectileSpeed = Vector2.up;
+                    rotation = Quaternion.Euler(0,0,90);
+                    break;
+                default:
+                    projectileSpeed = Vector2.right;
+                    break;
+                 
+            }
+
+            var projectilGameObject  = Instantiate(_projectilePrefab, transform.position, rotation);
+            var projectile = projectilGameObject.GetComponent<Projectile>();
+            projectile.SetSpeed(projectileSpeed);
         }
     }
 }
